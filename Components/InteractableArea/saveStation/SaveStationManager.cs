ced18b352754c9f6778dbb4b0c4dbdc028a98e02
zikiney1 <henriquesouzacosta19@gmail.com>
2025-05-12 19:24:30 -0300

    mc

--- Components/InteractableArea/saveStation/SaveStationManager.cs
+++ Components/InteractableArea/saveStation/SaveStationManager.cs
@@ -7,6 +7,15 @@ public partial class SaveStationManager : Node
     [Export] public Player player;
 
 
+    /// <summary>
+    /// Updates the know save stations of the player.
+    /// <para>
+    /// This function takes an array of save station names and updates the player's known save stations.
+    /// </para>
+    /// <para>
+    /// If the given array is null or empty, the function does nothing.
+    /// </para>
+    /// </summary>
     public void UpdateKnows(string[] saveName){
         if(saveName == null) return;
         if(saveName.Length == 0) return;
